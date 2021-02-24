8aeefdffad89eaeb9913791a5349ad17a1f792d5
Egor Simchuk <e.simchuk@samsung.com>
2021-02-24 21:55:12 +0300

    use telegram lib

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
-import telebot
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import MessageHandler, Filters
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
 
 if __name__ == '__main__':
-    bot = telebot.TeleBot('1677667809:AAHH2KzZBHJl4NPS8Jqjrw4bMl6VYviQ8ac')
 
 
-    @bot.message_handler(commands=['start', 'help'])
-    def send_welcome(message):
-        bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+    updater = Updater('1677667809:AAHH2KzZBHJl4NPS8Jqjrw4bMl6VYviQ8ac')
+
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
 
-    @bot.message_handler(content_types=['text'])
-    def get_text_messages(message):
-        if message.text.lower() == 'привет':
-            bot.send_message(message.from_user.id, 'Привет!')
-        else:
-            bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
 
 
-    bot.polling(none_stop=True)
+    def caps(update, context):
+        text_caps = ' '.join(context.args).upper()
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+    caps_handler = CommandHandler('caps', caps)
+    dispatcher.add_handler(caps_handler)
+
+
+    def unknown(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Sorry, I didn't understand that command.")
+    unknown_handler = MessageHandler(Filters.command, unknown)
+    dispatcher.add_handler(unknown_handler)
+
+    updater.start_polling()
